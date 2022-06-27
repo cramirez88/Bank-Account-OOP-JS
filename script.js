6e25751340d575b55aa52378fa43dc8b7c960e2a
Christian Ramirez <christian.r0602@gmail.com>
2022-06-27 12:10:10 -0500

    updating methods with associations

--- script.js
+++ script.js
@@ -24,9 +24,37 @@ class BankAccount {
       }
 }
 
-const Mickey = new BankAccount( 0, 0.1)
-const Minnie = new BankAccount(100, 0.1)
-Mickey.deposit(100).deposit(100).deposit(100).withdraw(10).yieldInterest(0.1).displayAccountInfo()
-Minnie.deposit(1000).deposit(1000).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yieldInterest(0.1).displayAccountInfo()
-console.log(Minnie.balance)
+
+class User {
+  constructor(name, emailAddress){
+    this.name = name,
+    this.email = emailAddress,
+    this.account = new BankAccount(balance=0, interestRate=0.1)
+  }
+  makeDeposit(amount){
+    this.account.deposit += amount
+    return this
+  }
+
+  makeWithdrawal(amount){
+    this.account.withdraw -= amount
+    return this
+  }
+
+  displayBalance(){
+    console.log(this.name +' has ' + this.account.displayAccountInfo + ' dollars in the bank')
+  }
+  transferMoney(amount, account){
+    this.accountBalance -= amount
+    account.accountBalance += amount
+  }
+}
+
+const Mickey = new BankAccount (balance=90, interestRate=0.1)
+
+// const Minnie = new BankAccount(100, 0.1)
+// Mickey.deposit(100).deposit(100).deposit(100).withdraw(10).yieldInterest(0.1).displayAccountInfo()
+// Minnie.deposit(1000).deposit(1000).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yieldInterest(0.1).displayAccountInfo()
+// console.log(Minnie.balance)
+console.log(Mickey.displayAccountInfo())
 
